3ff6cfef04065b630e1f30f14a05bd75cf704ac0
avrabie <mr.vrabie@gmail.com>
2022-09-24 23:10:06 +0200

    corrected a comment

--- src/main/java/org/charnele/shop/model/Food.java
+++ src/main/java/org/charnele/shop/model/Food.java
@@ -5,7 +5,7 @@ public abstract class Food implements FoodItem {
     @Override
     public String toString() {
         int length = 80;
-        String currencySymbol = " CHF"; //for $ or other symbols, no need to put a space at the end
+        String currencySymbol = " CHF"; //for $ or other symbols, no need to put a space in front
         length = length - this.getDescription().length() ;
         String formatPaddingPrice = String.format("%1$" + length + "s", this.getPrice());
         return String.format("%s", this.getDescription() + formatPaddingPrice + currencySymbol);
